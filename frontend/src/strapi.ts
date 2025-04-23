ada5a2175006ce8043beb2619c3bf1c8e4c85474
Oleg Omelchenko <oleg.omelc93@gmail.com>
2025-04-23 12:55:05 +0300

    init

--- frontend/src/strapi.ts
+++ frontend/src/strapi.ts
@@ -2,6 +2,6 @@ import { Image } from "@/interfaces/strapi";
 
 export const getImage = (img: Image) => {
   return img?.url
-    ? `${process.env.NEXT_PUBLIC_API_URL}${img.url}`
+    ? `${process.env.NEXT_PUBLIC_DOMAIN}${img.url}`
     : "/no-image.png";
 };
